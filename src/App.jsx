3055ea1377e072c6cfeaf804dbc59968f54ddefb
aemirdnr <aemirdnr@gmail.com>
2022-10-15 05:41:40 +0300

    Remove: dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import React from "react";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#FFF",
+        dark: "#FFF",
+      },
+    },
+  });
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <Content />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./style.css";
 
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+
 import { ApolloProvider } from "@apollo/client";
 import client from "./data/client";
 
