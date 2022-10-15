06611f7c601fc79be785f591a0e5370a1e624b71
aemirdnr <aemirdnr@gmail.com>
2022-10-15 05:45:49 +0300

    Remove: reateTheme, ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import { createTheme, ThemeProvider } from "@mui/material";
-import React from "react";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#FFF",
-        dark: "#FFF",
-      },
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Header />
       <Content />
-    </ThemeProvider>
+    </>
   );
 }
 
